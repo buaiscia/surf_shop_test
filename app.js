bb749647a429cd1869366df25a7504b3181e8e7e
buaiscia <alex.buaiscia@gmail.com>
2019-05-09 16:46:56 +0200

    added passport and session

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// const bodyParser = require('body-parser');
+const passport = require('passport')
+const User = require('./models/user');
 
+//require routes
 const indexRouter   = require('./routes/index');
 const postsRouter   = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
@@ -20,6 +24,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Configure Passport and sessions
+
+app.use(session({
+  secret: 'Sahaja Yoga',
+  resave: false,
+  saveUninitialized: true
+}))
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Mount routes
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
