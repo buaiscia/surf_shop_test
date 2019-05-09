ae827fb5db13554a9f4f1093d69cdb05a11d7952
buaiscia <alex.buaiscia@gmail.com>
2019-05-09 15:10:56 +0200

    added post routes

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,37 @@ const router = express.Router();
 
 /* GET post index /posts */
 router.get('/', (req, res, next) => {
-    res.send('/posts');
+    res.send('INDEX /posts');
+});
+
+/* GET post new /posts/new */
+router.get('/new', (req, res, next) => {
+    res.send('NEW /posts/new');
+});
+
+/* POST post create /posts */
+router.post('/', (req, res, next) => {
+    res.send('CREATE /posts');
+});
+
+/* GET post show /posts/:id */
+router.get('/:id', (req, res, next) => {
+    res.send('SHOW /posts/:id');
+});
+
+/* GET post edit /posts/:id/edit */
+router.get('/:id/edit', (req, res, next) => {
+    res.send('EDIT /posts/:id/edit');
+});
+
+/* PUT post update /posts/:id */
+router.put('/:id', (req, res, next) => {
+    res.send('UPDATE /posts/:id');
+});
+
+/* DELETE  post delete /posts/:id */
+router.delete('/:id', (req, res, next) => {
+    res.send('DELETE /posts/:id');
 });
 
 module.exports = router;
